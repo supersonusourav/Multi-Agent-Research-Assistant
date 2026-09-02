f972393397a472977a227636fcce7950caaeab65
Sonu Sourav <78981483+supersonusourav@users.noreply.github.com>
2026-09-02 10:31:14 +0530

    Uncomment load_dotenv function call

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 # from langchain_mistralai import ChatMistralAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 
- load_dotenv()
+load_dotenv()
 
 # llm = ChatMistralAI(
 #     model_name="mistral-large-latest",
